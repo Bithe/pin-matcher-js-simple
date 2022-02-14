27f4c90779ce176325cb3f0035798dfa35aae152
bithe <farzana.hossain5@gmail.com>
2022-02-14 16:19:54 +0600

    function done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        console.log("Got 3 digit", pin);
+        return getPin();
+
+    }
 }
 
 function generatePin() {
-    getPin();
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
     console.log(pin);
 }
